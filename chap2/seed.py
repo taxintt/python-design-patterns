3f852a40fbc7614f3deb1e20a583f24e15f6a227
taxintt <nthouse14@gmail.com>
2023-01-11 13:41:27 +0900

    Rename directory and add seed.py

---
+++ chap2/seed.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+_seed = datetime.now().microsecond % 255 + 1
+
+def set_seed(value):
+    global _seed
+    _seed = value
+
+def random():
+    global _seed
+    _seed, carry = divmod(_seed, 2)
+    if carry:
+        _seed ^= 0xb8
+    return _seed
+
+if __name__ == "__main__":
+    print(random())
